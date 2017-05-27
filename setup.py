67a82579fd61590fce539a1979b07f2fad0fa774
Steven Diamond <stevend2@stanford.edu>
2017-05-27 15:15:17 -0700

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ncvx',
-    version='0.1.4',
+    version='0.1.5',
     author='Steven Diamond, Reza Takapoui, Stephen Boyd',
      author_email='diamond@cs.stanford.edu, takapoui@stanford.edu, boyd@stanford.edu',
     packages=['ncvx'],
