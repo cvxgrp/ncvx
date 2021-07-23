db8fdb5428c00db8b315eb6f07c371967e4cf9ec
bettbra <bradley.betts@gmail.com>
2021-07-22 22:45:07 -0700

    Convert to pytest. As a bonus, the parallel solving now seems to work.

--- ncvx/tests/test_vars.py
+++ ncvx/tests/test_vars.py
@@ -18,79 +18,80 @@ along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import cvxpy as cp
+import pytest
 from cvxpy import sum, Minimize, Maximize, Problem, Variable
 from ncvx import *
 import numpy as np
-import unittest
 from numpy.testing import assert_array_almost_equal
 
+solve_args = {'method': 'NC-ADMM', 'solver': cp.SCS, 'parallel': True,
+              'verbose': False}
 
-class TestVars(unittest.TestCase):
-    """ Unit tests for the variable types. """
-    def setUp(self):
-        self._places = 5      # tune the tolerance down a bit as part of CVDOPT -> SCS switch
-        # Parallel running during unit testing seems to cause some snags. So set
-        # to false here.
-        self._solve = {'method': 'NC-ADMM', 'solver': cp.SCS, 'parallel': False,
-                       'verbose': False}
 
-    def test_boolean(self):
-        """Test boolean variable."""
-        x = Variable((5, 4))
-        y = Boolean((5, 4))
-        p = Problem(Minimize(sum(1 - x) + sum(x)), [x == y])
-        result = p.solve(**self._solve)
-        self.assertAlmostEqual(result[0], 20, places=self._places)
-        for v in np.nditer(x.value):
-            self.assertAlmostEqual(v * (1 - v), 0, places=self._places)
+def approx(x, abs=1e-5, *args, **kwargs):
+    return pytest.approx(x, abs=abs, *args, **kwargs)
 
-        # This time test a scalar variable, while restricting to a single entry
-        # of a Boolean matrix.
-        x = Variable()
-        p = Problem(Minimize(sum(1 - x) + sum(x)), [x == Boolean((5, 4))[0, 0]])
-        result = p.solve(**self._solve)
-        self.assertAlmostEqual(result[0], 1, places=self._places)
-        self.assertAlmostEqual(x.value * (1 - x.value), 0, places=self._places)
 
-    def test_choose(self):
-        """Test choose variable."""
-        x = Variable((5, 4))
-        y = Choose((5, 4), k=4)
-        p = Problem(Minimize(sum(1 - x) + sum(x)), [x == y])
-        result = p.solve(**self._solve)
-        self.assertAlmostEqual(result[0], 20, places=self._places)
-        for v in np.nditer(x.value):
-            self.assertAlmostEqual(v * (1 - v), 0, places=self._places)
-        self.assertAlmostEqual(x.value.sum(), 4, places=self._places)
+def test_boolean():
+    """Test boolean variable."""
+    x = Variable((5, 4))
+    y = Boolean((5, 4))
+    p = Problem(Minimize(sum(1 - x) + sum(x)), [x == y])
+    result = p.solve(**solve_args)
+    assert result[0] == approx(20)
+    for v in np.nditer(x.value):
+        assert v * (1 - v) == approx(0)
 
-    # Test card variable.
-    def test_card(self):
-        x = Card(5, k=3, M=1)
-        p = Problem(Maximize(sum(x)), [x <= 1, x >= 0])
-        result = p.solve(**self._solve)
+    # This time test a scalar variable, while restricting to a single entry
+    # of a Boolean matrix.
+    x = Variable()
+    p = Problem(Minimize(sum(1 - x) + sum(x)), [x == Boolean((5, 4))[0, 0]])
+    result = p.solve(**solve_args)
+    assert result[0] == approx(1)
+    assert x.value * (1 - x.value) == approx(0)
 
-        self.assertAlmostEqual(result[0], 3, places=self._places)
-        for v in np.nditer(x.value):
-            self.assertAlmostEqual(v * (1 - v), 0, places=self._places)
-        self.assertAlmostEqual(x.value.sum(), 3, places=self._places)
 
-        # Should be equivalent to x == choose.
-        x = Variable((5, 4))
-        c = Choose((5, 4), k=4)
-        b = Boolean((5, 4))
-        p = cp.Problem(Minimize(sum(1 - x) + sum(x)),
-                       [x == c, x == b])
-        result = p.solve(**self._solve)
-        self.assertAlmostEqual(result[0], 20, places=self._places)
-        for v in np.nditer(x.value):
-            self.assertAlmostEqual(v * (1 - v), 0, places=self._places)
+def test_choose():
+    """Test choose variable."""
+    x = Variable((5, 4))
+    y = Choose((5, 4), k=4)
+    p = Problem(Minimize(sum(1 - x) + sum(x)), [x == y])
+    result = p.solve(**solve_args)
+    assert result[0] == approx(20)
+    for v in np.nditer(x.value):
+        assert v * (1 - v) == approx(0)
+    assert x.value.sum() == approx(4)
 
-    # Test permutation variable.
-    def test_permutation(self):
-        x = Variable((1, 5))
-        c = np.array([[1, 2, 3, 4, 5]])
-        perm = Assign((5, 5))
-        p = Problem(Minimize(sum(x)), [x == c*perm])
-        result = p.solve(**self._solve)
-        self.assertAlmostEqual(result[0], 15, places=self._places)
-        assert_array_almost_equal(sorted(np.nditer(x.value)), c.ravel())
+
+def test_card():
+    """Test card variable."""
+    x = Card(5, k=3, M=1)
+    p = Problem(Maximize(sum(x)), [x <= 1, x >= 0])
+    result = p.solve(**solve_args)
+
+    assert result[0] == approx(3)
+    for v in np.nditer(x.value):
+        assert v * (1 - v) == approx(0)
+    assert x.value.sum() == approx(3)
+
+    # Should be equivalent to x == choose.
+    x = Variable((5, 4))
+    c = Choose((5, 4), k=4)
+    b = Boolean((5, 4))
+    p = cp.Problem(Minimize(sum(1 - x) + sum(x)),
+                   [x == c, x == b])
+    result = p.solve(**solve_args)
+    assert result[0] == approx(20)
+    for v in np.nditer(x.value):
+        assert v * (1 - v) == approx(0)
+
+
+def test_permutation():
+    """Test permutation variable."""
+    x = Variable((1, 5))
+    c = np.array([[1, 2, 3, 4, 5]])
+    perm = Assign((5, 5))
+    p = Problem(Minimize(sum(x)), [x == c * perm])
+    result = p.solve(**solve_args)
+    assert result[0] == approx(15)
+    assert_array_almost_equal(sorted(np.nditer(x.value)), c.ravel())
