8ef0fd6cef8cee691606f34bad9b02c2324a2711
bettbra <bradley.betts@gmail.com>
2021-08-01 14:46:15 -0700

    Modify the imports to current cvxpy standards. Fix a * -> @ multiply. Minor output format mods.

--- examples/factor_model.py
+++ examples/factor_model.py
@@ -1,5 +1,4 @@
 import cvxpy as cp
-from cvxpy import norm, Variable, diag, sum_squares, Minimize, trace, Problem, Parameter, pos
 import ncvx as nc
 import numpy as np
 
@@ -15,17 +14,17 @@ SNR = 20
 F = rng.standard_normal(size=(n, k))
 D_true = rng.exponential(1, size=(n, 1))
 Sigma_true = F @ F.T + np.diag(D_true)
-variance = norm(Sigma_true, 'fro').value / (np.sqrt(n * n) * SNR)
+variance = cp.norm(Sigma_true, 'fro').value / (np.sqrt(n * n) * SNR)
 noise = rng.normal(0, variance, size=(n, n))
 Sigma = Sigma_true + (noise + noise.T) / 2
 
 # NC-ADMM heuristic
 Sigma_lr = nc.Rank((n, n), k, M=None, symmetric=True)
-D_vec = Variable(n)
-D = diag(D_vec)
-cost = sum_squares(Sigma - Sigma_lr - D)
+D_vec = cp.Variable(n)
+D = cp.diag(D_vec)
+cost = cp.sum_squares(Sigma - Sigma_lr - D)
 constraints = [D_vec >= 0, Sigma_lr >= 0]
-prob = Problem(Minimize(cost), constraints)
+prob = cp.Problem(cp.Minimize(cost), constraints)
 
 
 def polish_func(sltn):
@@ -34,13 +33,13 @@ def polish_func(sltn):
     w_sorted_idxs = np.argsort(-w)
     pos_w = w[w_sorted_idxs[:k]]
     pos_V = V[:, w_sorted_idxs[:k]]
-    Sigma_tmp = Variable((k, k), symmetric=True)
-    Sigma_small = pos_V * Sigma_tmp * pos_V.T
+    Sigma_tmp = cp.Variable((k, k), symmetric=True)
+    Sigma_small = pos_V @ Sigma_tmp @ pos_V.T
 
-    D_vec2 = Variable(n); D = diag(D_vec2)
-    cost = sum_squares(Sigma - Sigma_small - D)
+    D_vec2 = cp.Variable(n); D = cp.diag(D_vec2)
+    cost = cp.sum_squares(Sigma - Sigma_small - D)
     constraints = [D_vec >= 0, Sigma_lr >> 0]
-    prob = Problem(Minimize(cost), constraints)
+    prob = cp.Problem(cp.Minimize(cost), constraints)
     result = prob.solve(solver=cp.SCS)
     return result, {Sigma_lr.id: Sigma_small.value, D_vec.id: D_vec2.value}
 
@@ -48,27 +47,27 @@ def polish_func(sltn):
 prob.solve(method="NC-ADMM", solver=cp.SCS, show_progress=True, parallel=False,
            restarts=1, max_iter=10, polish_func=polish_func, polish_depth=10)
 Sigma_lr.value = Sigma_lr.project(Sigma_lr.value)
-D_vec.value = pos(D_vec).value
-print("NC-ADMM value", cost.value)
+D_vec.value = cp.pos(D_vec).value
+print(f"NC-ADMM value = {cost.value}")
 
 w, V = np.linalg.eigh(Sigma_lr.value)
-print(w, D_vec.value)
-print(sum_squares(Sigma - Sigma_lr - D).value)
-print(sum_squares(Sigma - Sigma_lr.project(Sigma)).value)
+print(f"w = \n{w}\nD = \n{D_vec.value}")
+print(cp.sum_squares(Sigma - Sigma_lr - D).value)
+print(cp.sum_squares(Sigma - Sigma_lr.project(Sigma)).value)
 
 # Relax-round-polish heuristic
 prob.solve(method="relax-round-polish", solver=cp.SCS)
-print("Relax-round-polish value", cost.value)
+print(f"Relax-round-polish value = {cost.value}")
 
 # Nuclear norm heuristic
-gamma = Parameter(nonneg=True)
-Sigma_lr = Variable((n, n), PSD=True)
-reg = trace(Sigma_lr)
-D_vec = Variable(n)
-D = diag(D_vec)
-cost = sum_squares(Sigma - Sigma_lr - D)
+gamma = cp.Parameter(nonneg=True)
+Sigma_lr = cp.Variable((n, n), PSD=True)
+reg = cp.trace(Sigma_lr)
+D_vec = cp.Variable(n)
+D = cp.diag(D_vec)
+cost = cp.sum_squares(Sigma - Sigma_lr - D)
 constraints = [D_vec >= 0]
-prob = Problem(Minimize(cost + gamma*reg), constraints)
+prob = cp.Problem(cp.Minimize(cost + gamma*reg), constraints)
 found_lr = False
 for gamma_val in np.logspace(-2, 2, num=100):
     gamma.value = gamma_val
@@ -77,9 +76,9 @@ for gamma_val in np.logspace(-2, 2, num=100):
     rank = sum(w > 1e-3)
     if rank <= k:
         # Polish.
-        polish_prob = Problem(Minimize(cost),
-                              constraints + [Sigma_lr == Sigma_lr.value])
+        polish_prob = cp.Problem(cp.Minimize(cost),
+                                 constraints + [Sigma_lr == Sigma_lr.value])
         found_lr = True
         break
 assert found_lr
-print("Nuclear norm value", cost.value)
+print(f"Nuclear norm value = {cost.value}")
